134538b7341df4de1352770e0b68f21b5aa5f4f8
Matheus Braynner <m.braynner@outlook.com>
2024-05-01 14:06:16 -0300

    first commit

---
+++ src/main/java/br/com/grupoconexao/msinvolved/MsInvolvedApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupoconexao.msinvolved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInvolvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInvolvedApplication.class, args);
+	}
+
+}
